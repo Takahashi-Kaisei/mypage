5dd1054f787a1410eeac04f4c7573beec90bb85a
Takahashi-Kaisei <s2322037@stu.musashino-u.ac.jp>
2025-03-20 14:36:46 +0900

    SpeakerDeckThumbnailコンポーネントにカスタムサムネイルURLのオプションを追加し、プロフィールページのテキストを整形しました

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -7,6 +7,7 @@ export const articles: Article[] = [
     title: "線形回帰の実装",
     description: "線形回帰の理論と実装についてまとめた．",
     speakerdeckUrl: "https://speakerdeck.com/kai_ds04/implementing-linear-regression",
+    thumbnailUrl: "https://speakerdeck.com/kai_ds04/implementing-linear-regression",
     tags: ["機械学習", "データサイエンス", "統計", "線形回帰"],
     date: "2024-05-27",
   }
