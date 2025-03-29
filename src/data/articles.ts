4b755411e706c407c1b8767def7384616f7336d8
Takahashi-Kaisei <s2322037@stu.musashino-u.ac.jp>
2025-03-30 01:25:05 +0900

    新しい記事「ロジスティック回帰の実装」を追加

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -9,5 +9,13 @@ export const articles: Article[] = [
     speakerdeckUrl: "https://speakerdeck.com/kai_ds04/implementing-linear-regression",
     tags: ["機械学習", "データサイエンス", "統計", "線形回帰"],
     date: "2024-05-27",
+  },
+  {
+    id: "2",
+    title: "ロジスティック回帰の実装",
+    description: "ロジスティック回帰の理論と実装についてまとめた．",
+    speakerdeckUrl: "https://speakerdeck.com/kai_ds04/rozisuteitukuhui-gui-noshi-zhuang",
+    tags: ["機械学習", "データサイエンス", "統計", "線形回帰", "ロジスティック回帰"],
+    date: "2025-03-30",
   }
 ];
